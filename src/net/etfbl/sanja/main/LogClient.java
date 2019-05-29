8d00d4a8d0062579451665e307763e6d066dcb2f
Sanja Vadic <sanja.vadic@gmail.com>
2019-05-29 17:06:38 +0200

    sitne izm

--- src/net/etfbl/sanja/main/LogClient.java
+++ src/net/etfbl/sanja/main/LogClient.java
@@ -8,7 +8,6 @@ import java.net.URL;
 
 import org.json.JSONObject;
 
-import com.mashape.unirest.http.Unirest;
 
 public class LogClient {
 	
@@ -19,12 +18,16 @@ public class LogClient {
 			root.put("requestMethod", log.getRequestMethod());
 			root.put("attackType", log.getAttackType());
 			
+			System.out.println("ServerProxyJSON: " + root.toString());
+			
 			URL url = new URL("http://localhost:8080/SigurnostSafe/EmptyLogServlet");
 			HttpURLConnection  conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("POST");
 			conn.setDoOutput(true); //Potrebno ako se salje request body
 			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-			out.write(root.toString().getBytes()); //npr. paramsStr = "name=Ime&lastname=Prezime";
+			//out.write(root.toString().getBytes()); //npr. paramsStr = "name=Ime&lastname=Prezime";
+			out.writeBytes(root.toString()); //npr. paramsStr = "name=Ime&lastname=Prezime";
+			out.flush();
 			out.close();
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			StringBuffer response = new StringBuffer();
